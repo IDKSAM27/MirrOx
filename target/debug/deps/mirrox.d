0c4a8e9336226ce5763b6a379926d4667ae7b2ac
Sampreet Patil <sampreetpatil270@gmail.com>
2025-03-28 15:22:17 +0530

    Remove .exe, .pdb, and target/ files from the repository

--- target/debug/deps/mirrox.d
+++
@@ -1,7 +0,0 @@
-D:\Projects\MirrOx\target\debug\deps\mirrox.exe: src\main.rs src\utils.rs src\adb.rs
-
-D:\Projects\MirrOx\target\debug\deps\mirrox.d: src\main.rs src\utils.rs src\adb.rs
-
-src\main.rs:
-src\utils.rs:
-src\adb.rs:
