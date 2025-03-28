523e1b05480fee087141f82cca59f2fcaaaa69d6
Sampreet Patil <sampreetpatil270@gmail.com>
2025-03-28 15:16:29 +0530

    .gitignore added

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 pub fn extract_battery_level(output: &str) -> String {
     for line in output.lines() {
-        if let Some(level) = line.trim().strip_prefix("level: ") {
+        if let Some(level) = line.trim().strip_prefix("level: ") {  // line.trim() was not present, hence it was not able to trim the leading white spaces in the total battery info
             return level.trim().to_string();
         }
     }
