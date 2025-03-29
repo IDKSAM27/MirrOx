2724142cb90dfbe85294ef24537f5eb385a0a945
Sampreet Patil <sampreetpatil270@gmail.com>
2025-03-29 22:39:26 +0530

    git rm -r --cached temp/

--- target/debug/deps/mirrox-f39f0b9c51d5eabc.d
+++
@@ -1,7 +0,0 @@
-D:\Projects\MirrOx\target\debug\deps\libmirrox-f39f0b9c51d5eabc.rmeta: src\main.rs src\utils.rs src\adb.rs
-
-D:\Projects\MirrOx\target\debug\deps\mirrox-f39f0b9c51d5eabc.d: src\main.rs src\utils.rs src\adb.rs
-
-src\main.rs:
-src\utils.rs:
-src\adb.rs:
