8c72c6fe0a3d08e5f5ccdf3d093eeca1f9461faf
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-04 17:29:02 +0530

    updated mainactivity and screenstreamingservice, lot's of bugs though

--- android_server/app/src/main/java/com/example/android_server/ScreenCaptureService.java
+++ android_server/app/src/main/java/com/example/android_server/ScreenCaptureService.java
@@ -1,8 +1,5 @@
 package com.example.android_server;
 
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
@@ -10,87 +7,100 @@ import android.hardware.display.DisplayManager;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.media.ImageReader;
+import android.media.Image;
+import java.nio.ByteBuffer;
+import java.io.ByteArrayOutputStream;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.IOException;
 
 public class ScreenCaptureService extends Service {
-    private static final int PORT = 8080;
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private OutputStream outputStream;
     private MediaProjection mediaProjection;
-    private SurfaceView surfaceView;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        startServer();
-    }
-
-    private void startServer() {
-        new Thread(() -> {
-            try {
-                serverSocket = new ServerSocket(PORT);
-                clientSocket = serverSocket.accept();
-                outputStream = clientSocket.getOutputStream();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
-    private void sendFrame(byte[] data) {
-        try {
-            if (outputStream != null) {
-                outputStream.write(data);
-                outputStream.flush();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        MediaProjectionManager projectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
-        int resultCode = intent.getIntExtra("RESULT_CODE", -1);
-        Intent data = intent.getParcelableExtra("DATA");
-
-        if (projectionManager != null && resultCode != -1 && data != null) {
-            mediaProjection = projectionManager.getMediaProjection(resultCode, data);
-            setupVirtualDisplay();
-        }
-        return START_STICKY;
-    }
-
-    private void setupVirtualDisplay() {
-        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        surfaceView = new SurfaceView(this);
-        Surface surface = surfaceView.getHolder().getSurface();
-
-        mediaProjection.createVirtualDisplay("ScreenCapture",
-                1080, 1920, 1,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                surface, null, null);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        try {
-            if (clientSocket != null) clientSocket.close();
-            if (serverSocket != null) serverSocket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (mediaProjection != null) {
-            mediaProjection.stop();
-        }
-    }
+    private ImageReader imageReader;
+    private ScreenStreamingServer streamingServer;
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int resultCode = intent.getIntExtra("RESULT_CODE", -1);
+        Intent data = intent.getParcelableExtra("DATA");
+
+        MediaProjectionManager mediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        if (mediaProjectionManager != null) {
+            mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        }
+
+        if (mediaProjection != null) {
+            startScreenCapture();
+        }
+
+        // Start the server
+        streamingServer = new ScreenStreamingServer();
+        streamingServer.startServer();
+
+        return START_STICKY;
+    }
+
+    private void startScreenCapture() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        if (windowManager != null) {
+            windowManager.getDefaultDisplay().getMetrics(metrics);
+        }
+
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        imageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 2);
+        Surface surface = imageReader.getSurface();
+        mediaProjection.createVirtualDisplay("ScreenCapture",
+                width, height, metrics.densityDpi,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                surface, null, null);
+
+        imageReader.setOnImageAvailableListener(reader -> {
+            Image image = reader.acquireLatestImage();
+            if (image != null) {
+                sendFrame(image);
+                image.close();
+            }
+        }, null);
+    }
+
+    private void sendFrame(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer buffer = planes[0].getBuffer();
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(buffer);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+        byte[] jpegData = outputStream.toByteArray();
+
+        streamingServer.sendFrame(jpegData);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaProjection != null) {
+            mediaProjection.stop();
+        }
+        if (streamingServer != null) {
+            streamingServer.stopServer();
+        }
+    }
 }
+
