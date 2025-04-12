e12c4c910d023bfb511b7b0e72d105852f397c86
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-12 13:44:44 +0530

    Final commit for the PR, logging statements updated.

--- server/mirrox_server_jar/src/main/java/com/mirrox/server/MirroxServer.java
+++ server/mirrox_server_jar/src/main/java/com/mirrox/server/MirroxServer.java
@@ -2,17 +2,17 @@ package com.mirrox.server;
 
 public class MirroxServer {
     public static void main(String[] args) {
-        System.out.println("✅ MirrOx Server Started using main()");
+        System.out.println("MirrOx Server Started using main()");
 
         // Simulate long-running server or start your service logic
         try {
             // If needed, simulate Android-like environment (with Looper, etc.)
-            System.out.println("👀 Running MirrOx Server logic...");
+            System.out.println("Running MirrOx Server logic...");
 
             // Simulate running (replace with MediaProjection logic)
             Thread.sleep(10000);
 
-            System.out.println("✅ MirrOx Server Finished.");
+            System.out.println("MirrOx Server Finished.");
         } catch (Exception e) {
             e.printStackTrace();
         }
