2b5d4b29120534f101b49612a94623fc68cec899
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-12 12:50:03 +0530

    Debug lines and Log.i statements added for more detailed logcat exploration

--- server/mirrox_server_jar/src/main/java/com/mirrox/server/MirroxServer.java
+++ server/mirrox_server_jar/src/main/java/com/mirrox/server/MirroxServer.java
@@ -1,8 +1,11 @@
 package com.mirrox.server;
 
+import android.util.Log;
+
 public class MirroxServer {
     public static void main(String[] args) {
         try {
+            Log.i("MirroxServer", "Hello from Mirrox Server!");
             System.out.println("MirrOx server started");
 
             // Simulate long-running server
