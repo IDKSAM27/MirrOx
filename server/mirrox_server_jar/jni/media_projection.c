ea5861d0baecea33dfea73dd9d17168cb4ac2184
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-17 10:18:01 +0530

    c properties changed and up to date updation, aarch64 dir added in the prop

--- server/mirrox_server_jar/jni/media_projection.c
+++ server/mirrox_server_jar/jni/media_projection.c
@@ -2,6 +2,8 @@
 #include "media_projection.h"
 #include <stdio.h>
 
+#include <jni.h>
+
 JNIEXPORT jobject JNICALL
 Java_com_mirrox_server_StartMirrox_getMediaProjectionTokenNative(JNIEnv *env, jclass clazz) {
     // TODO: implement raw binder transaction like scrcpy
