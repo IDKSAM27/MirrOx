20eda95af2a8f1811461482a706a2ebbfbfe0361
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-17 10:36:48 +0530

    jni .so file compiled and saved

---
+++ server/mirrox_server_jar/obj/local/armeabi-v7a/objs/mirroxjni/media_projection.o.d
@@ -0,0 +1,4 @@
+D:/Projects/MirrOx/server/mirrox_server_jar/obj/local/armeabi-v7a/objs/mirroxjni/media_projection.o: \
+  D:/Projects/MirrOx/server/mirrox_server_jar/jni/media_projection.c \
+  D:/Projects/MirrOx/server/mirrox_server_jar/jni/media_projection.h
+D:/Projects/MirrOx/server/mirrox_server_jar/jni/media_projection.h:
--- server/mirrox_server_jar/src/main/java/com/mirrox/server/StartMirrox.java
+++ server/mirrox_server_jar/src/main/java/com/mirrox/server/StartMirrox.java
@@ -2,31 +2,42 @@ package com.mirrox.server;
 
 import android.content.Context;
 import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.IBinder;
-import android.os.IInterface;
 import android.os.Looper;
-import android.os.Parcel;
-
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class StartMirrox {
 
-    private static final int SCREEN_WIDTH = 720;
-    private static final int SCREEN_HEIGHT = 1280;
+    static {
+        System.loadLibrary("mirroxjni"); // libmirroxjni.so
+    }
+
+    public static native IBinder getMediaProjectionTokenNative();
 
     public static void main(String[] args) {
         System.out.println("✅ MirrOx Server Started using main()");
+
         try {
             Context context = getSystemContext();
-            MediaProjection mediaProjection = getMediaProjectionFromBinder(context);
-            if (mediaProjection == null) {
-                System.err.println("❌ Failed to get MediaProjection");
+
+            IBinder projectionToken = getMediaProjectionTokenNative();
+            if (projectionToken == null) {
+                System.err.println("❌ Failed to obtain projection token");
                 return;
             }
 
-            ScreenEncoder encoder = new ScreenEncoder(mediaProjection);
-            encoder.start(SCREEN_WIDTH, SCREEN_HEIGHT);
+            MediaProjectionManager mpm =
+                    (MediaProjectionManager) context.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+            Method getProjection = MediaProjectionManager.class.getDeclaredMethod(
+                    "getMediaProjection", int.class, IBinder.class);
+            getProjection.setAccessible(true);
+            MediaProjection mp = (MediaProjection) getProjection.invoke(mpm, 0, projectionToken);
+
+            // start encoder
+            ScreenEncoder encoder = new ScreenEncoder(mp);
+            encoder.start(720, 1280);
 
             while (true) {
                 Thread.sleep(1000);
@@ -39,50 +50,11 @@ public class StartMirrox {
 
     private static Context getSystemContext() throws Exception {
         Looper.prepareMainLooper();
-
-        Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
-        Method systemMain = activityThreadClass.getMethod("systemMain");
-        Object activityThread = systemMain.invoke(null);
-
-        Method getSystemContext = activityThreadClass.getMethod("getSystemContext");
-        return (Context) getSystemContext.invoke(activityThread);
-    }
-
-    private static MediaProjection getMediaProjectionFromBinder(Context context) {
-        try {
-            IBinder binder = (IBinder) Class.forName("android.os.ServiceManager")
-                    .getMethod("getService", String.class)
-                    .invoke(null, Context.MEDIA_PROJECTION_SERVICE);
-
-            if (binder == null) {
-                System.err.println("❌ Could not get media_projection service");
-                return null;
-            }
-
-            Parcel data = Parcel.obtain();
-            Parcel reply = Parcel.obtain();
-
-            data.writeInterfaceToken("android.media.projection.IMediaProjectionManager");
-            data.writeInt(2000); // UID for shell
-            data.writeString("com.mirrox.server");
-
-            binder.transact(1, data, reply, 0);
-            reply.readException();
-            IBinder projectionBinder = reply.readStrongBinder();
-
-            // Bind to IMediaProjection
-            Class<?> stubClass = Class.forName("android.media.projection.IMediaProjection$Stub");
-            Method asInterface = stubClass.getMethod("asInterface", IBinder.class);
-            IInterface iMediaProjection = (IInterface) asInterface.invoke(null, projectionBinder);
-
-            // Use hidden constructor of MediaProjection
-            Constructor<MediaProjection> constructor = MediaProjection.class.getDeclaredConstructor(int.class, IInterface.class);
-            constructor.setAccessible(true);
-            return constructor.newInstance(0, iMediaProjection);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        Class<?> activityThread = Class.forName("android.app.ActivityThread");
+        Method systemMain = activityThread.getMethod("systemMain");
+        Object at = systemMain.invoke(null);
+        Method getContext = activityThread.getMethod("getSystemContext");
+        return (Context) getContext.invoke(at);
     }
 }
+
