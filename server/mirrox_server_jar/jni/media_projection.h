75636cbd6c7cb2c2fe949b07c19dd40c8cf7dedd
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-18 14:19:58 +0530

    media_projection updated to return 42 upon the server activation

--- server/mirrox_server_jar/jni/media_projection.h
+++ server/mirrox_server_jar/jni/media_projection.h
@@ -1,10 +1,24 @@
 // media_projection.h
-#ifndef MIRROX_MEDIA_PROJECTION_H
-#define MIRROX_MEDIA_PROJECTION_H
+// #ifndef MIRROX_MEDIA_PROJECTION_H
+// #define MIRROX_MEDIA_PROJECTION_H
 
-#include <jni.h>
+// #include <jni.h>
 
-JNIEXPORT jobject JNICALL
-Java_com_mirrox_server_StartMirrox_getMediaProjectionTokenNative(JNIEnv *env, jclass clazz);
+// JNIEXPORT jobject JNICALL
+// Java_com_mirrox_server_StartMirrox_getMediaProjectionTokenNative(JNIEnv *env, jclass clazz);
 
+// #endif
+
+
+/* DO NOT include in build if you're already compiling the C file directly */
+
+#ifndef _Included_com_mirrox_server_StartMirrox
+#define _Included_com_mirrox_server_StartMirrox
+#ifdef __cplusplus
+extern "C" {
+#endif
+JNIEXPORT jint JNICALL Java_com_mirrox_server_StartMirrox_startMediaProjection(JNIEnv *, jclass);
+#ifdef __cplusplus
+}
+#endif
 #endif
