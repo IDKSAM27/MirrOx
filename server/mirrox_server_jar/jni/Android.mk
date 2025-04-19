a4d7ed3fb2b40c061c2baf641f87c5a6e27d73c0
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-19 19:31:33 +0530

    Major update of the binder includes from framework_native

--- server/mirrox_server_jar/jni/Android.mk
+++ server/mirrox_server_jar/jni/Android.mk
@@ -1,7 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE    := mirroxjni
 LOCAL_SRC_FILES := media_projection.c
-LOCAL_LDLIBS    := -llog
+
+LOCAL_LDLIBS := -llog -landroid
+
+# Adjusted AOSP header includes
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/../../platform_system_core/include \
+    $(LOCAL_PATH)/../../platform_system_core/libutils/include \
+    $(LOCAL_PATH)/../../platform_frameworks_base/core/java/android/os \
+    $(LOCAL_PATH)/../../platform_frameworks_base/core/java/android/content \
+    $(LOCAL_PATH)/../../platform_frameworks_base/media/java/android/media/projection
+
 include $(BUILD_SHARED_LIBRARY)
--- server/mirrox_server_jar/jni/media_projection.c
+++ server/mirrox_server_jar/jni/media_projection.c
@@ -1,28 +1,45 @@
-// media_projection.c
-#include "media_projection.h"
-#include <stdio.h>
-
 #include <jni.h>
-// #include <binder/IServiceManager.h>
-// #include <binder/Parcel.h>
-// #include <utils/String16.h>
-// #include <utils/StrongPointer.h>
+#include <stdio.h>
+#include <binder/IServiceManager.h>
+#include <binder/Parcel.h>
+#include <utils/String16.h>
+#include <utils/StrongPointer.h>
+#include <android/log.h>
 
-binder/IServiceManager.h
+#define TAG "MirrOxJNI"
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
-binder/IBinder.h
+using namespace android;
 
-utils/RefBase.h
-
-utils/String16.h
-
-
-// using namespace android;
-
-// Signature: Java_[package]_[ClassName]_methodname             , this is the only way to name the below function name
+extern "C"
 JNIEXPORT jobject JNICALL
-Java_com_mirrox_server_StartMirrox_startMediaProjection(JNIEnv *env, jclass clazz) {
-    // TODO: implement raw binder transaction like scrcpy
-    printf("Native function called!\n");
-    return 0;
+Java_com_mirrox_server_StartMirrox_getMediaProjectionTokenNative(JNIEnv *env, jclass clazz) {
+    sp<IBinder> service = defaultServiceManager()->getService(String16("media_projection"));
+    if (service == nullptr) {
+        ALOGE("❌ Failed to get media_projection service");
+        return nullptr;
+    }
+
+    Parcel data, reply;
+    data.writeInterfaceToken(String16("android.media.projection.IMediaProjectionManager"));
+
+    int32_t uid = 2000; // shell UID (must be run via app_process as shell)
+    String16 packageName("com.mirrox.server"); // Your server package name
+    int32_t type = 1; // TYPE_SCREEN_CAPTURE
+    bool permanentGrant = true;
+
+    data.writeInt32(uid);
+    data.writeString16(packageName);
+    data.writeInt32(type);
+    data.writeInt32(permanentGrant ? 1 : 0);
+
+    status_t status = service->transact(5 /* TRANSACTION_createProjection */, data, &reply);
+    if (status != NO_ERROR) {
+        ALOGE("❌ transact failed: %d", status);
+        return nullptr;
+    }
+
+    reply.readExceptionCode(); // ensure no remote exception
+    sp<IBinder> projectionToken = reply.readStrongBinder();
+    return env->NewLocalRef(reinterpret_cast<jobject>(projectionToken.get()));
 }
