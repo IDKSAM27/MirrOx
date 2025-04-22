005eb850b3be8cf4f9685a51e1b45d2a88515c3c
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-22 15:36:51 +0530

    ndk-build with appro. lib likers linked

--- server/mirrox_server_jar/jni/Android.mk
+++ server/mirrox_server_jar/jni/Android.mk
@@ -5,4 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := mirroxjni
 LOCAL_SRC_FILES := media_projection.c
 
+LOCAL_LDLIBS := -llog
+
 include $(BUILD_SHARED_LIBRARY)
---
+++ server/mirrox_server_jar/obj/local/x86_64/objs/mirroxjni/media_projection.o.d
@@ -0,0 +1,3 @@
+./obj/local/x86_64/objs/mirroxjni/media_projection.o: \
+  jni/media_projection.c jni/media_projection.h
+jni/media_projection.h:
