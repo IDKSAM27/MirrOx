321613ba97495b3bf77e9967a324ddb19031fcca
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-24 20:51:37 +0530

    Next steps added, more binder info added in the binder.txt

--- server/mirrox_server_jar/jni/include/binder_version.h
+++ server/mirrox_server_jar/jni/include/binder_version.h
@@ -1,5 +1,7 @@
 #ifndef MIRROX_BINDER_VERSION_H
 #define MIRROX_BINDER_VERSION_H
+#define IMEDIAPROJECTIONMANAGER_DESCRIPTOR "android.media.projection.IMediaProjectionManager"
+#define IMEDIAPROJECTIONMANAGER_CREATE_PROJECTION_TRANSACTION 1  // method index for createProjection() 
 
 #include <stdint.h>
 
