81ea7b44ea2c2e02cecb853b99133dbd07200b5a
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-26 17:30:33 +0530

    notes update

--- server/mirrox_server_jar/jni/binder_utils.c
+++ server/mirrox_server_jar/jni/binder_utils.c
@@ -1,21 +1,6 @@
+
 #define _GNU_SOURCE
 
-#include <stdio.h>   // <<<<<< Very important!
-
-#include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <sys/syscall.h>
-#include <errno.h>
-#include <sys/uio.h>
-#include <jni.h>
-
-// After all includes:
 #ifdef write
 #undef write
 #endif
@@ -24,40 +9,60 @@
 #undef read
 #endif
 
-#include "include/binder.h"
-#include "include/binderfs.h"
-#include "include/ioctl.h"
-#include "include/types.h"
-#include "include/binder_version.h"
-#include "binder_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <sys/uio.h>
+#include <jni.h>
 
+// Minimal fake binder headers to compile standalone
+#define BINDER_DEVICE "/dev/binder"
+#define BINDER_CURRENT_PROTOCOL_VERSION 8
+#define BINDER_VERSION _IOWR('b', 9, int)
 
-// Binder commands
+typedef uintptr_t binder_uintptr_t;
+typedef uint64_t binder_size_t;
+
 #define BC_TRANSACTION 0x5
 #define BR_TRANSACTION 0xC0000000
 
-// Correct binder_transaction_data
 struct binder_transaction_data {
-    union {
-        uint32_t handle;
-        uint64_t ptr;
-    } target;
-
+    binder_uintptr_t target;
     binder_uintptr_t cookie;
     uint32_t code;
     uint32_t flags;
 
     union {
         struct {
-            binder_uintptr_t buffer;
-            binder_uintptr_t offsets;
+            binder_uintptr_t ptr;
+            binder_size_t length;
         } ptr;
-        uint8_t buf[8];
+        struct {
+            uint64_t handle;
+            uint64_t cookie;
+        } u64;
     } data;
 
-    binder_size_t data_size;
     binder_size_t offsets_size;
+    binder_size_t data_size;
+    binder_uintptr_t data_buffer;
 };
+
+ssize_t safe_write(int fd, const void *buf, size_t count) {
+    return syscall(SYS_write, fd, buf, count);
+}
+
+ssize_t safe_read(int fd, void *buf, size_t count) {
+    return syscall(SYS_read, fd, buf, count);
+}
 
 int open_binder() {
     int fd = syscall(SYS_openat, AT_FDCWD, BINDER_DEVICE, O_RDWR | O_CLOEXEC);
@@ -81,41 +86,38 @@ int send_create_projection_transaction(int binder_fd, uint32_t handle) {
         uint32_t strict_mode;
         uint32_t interface_token_len;
         char interface_token[128];
-        uint32_t param1; // uid
+        uint32_t param1;
         uint32_t param2_len;
-        char param2[128]; // package name
-        uint32_t param3; // flags
-        uint32_t param4; // boolean
+        char param2[128];
+        uint32_t param3;
+        uint32_t param4;
     } __attribute__((packed)) data;
 
     memset(&data, 0, sizeof(data));
     data.strict_mode = 0x01000000;
-
     const char *descriptor = "android.media.projection.IMediaProjectionManager";
     data.interface_token_len = strlen(descriptor) + 1;
     strcpy(data.interface_token, descriptor);
-
-    data.param1 = getuid(); // calling UID
-
+    data.param1 = getuid();
     const char *pkg = "com.mirrox.server";
     data.param2_len = strlen(pkg) + 1;
     strcpy(data.param2, pkg);
-
-    data.param3 = 0; // flags
-    data.param4 = 0; // permanentGrant = false
+    data.param3 = 0;
+    data.param4 = 0;
 
     struct binder_transaction_data txn = {
-        .target.handle = handle,
-        .code = 1, // TRANSACTION_createProjection
+        .target = handle,
+        .code = 1,
         .flags = 0x00,
         .data = {
             .ptr = {
-                .buffer = (uintptr_t)&data,
-                .offsets = 0,
+                .ptr = (uintptr_t)&data,
+                .length = sizeof(data),
             },
         },
         .data_size = sizeof(data),
         .offsets_size = 0,
+        .data_buffer = 0,
     };
 
     struct {
@@ -126,7 +128,7 @@ int send_create_projection_transaction(int binder_fd, uint32_t handle) {
     write_buf.cmd = BC_TRANSACTION;
     write_buf.txn = txn;
 
-    ssize_t w = write(binder_fd, &write_buf, sizeof(write_buf));
+    ssize_t w = safe_write(binder_fd, &write_buf, sizeof(write_buf));
     if (w < 0) {
         perror("write BC_TRANSACTION failed");
         return -1;
@@ -137,7 +139,7 @@ int send_create_projection_transaction(int binder_fd, uint32_t handle) {
 
 jobject receive_media_projection_reply(int binder_fd, JNIEnv *env) {
     uint8_t buffer[1024];
-    ssize_t r = read(binder_fd, buffer, sizeof(buffer));
+    ssize_t r = safe_read(binder_fd, buffer, sizeof(buffer));
     if (r < 0) {
         perror("read BR_REPLY failed");
         return NULL;
@@ -146,18 +148,16 @@ jobject receive_media_projection_reply(int binder_fd, JNIEnv *env) {
     struct binder_transaction_data *txn_reply = NULL;
     size_t pos = 0;
 
-    while (pos + sizeof(uint32_t) < (size_t)r) {
+    while (pos + sizeof(uint32_t) < r) {
         uint32_t cmd = *(uint32_t *)(buffer + pos);
         pos += sizeof(uint32_t);
 
         if (cmd == BR_TRANSACTION) {
-            if (pos + sizeof(struct binder_transaction_data) <= (size_t)r) {
+            if (pos + sizeof(struct binder_transaction_data) <= r) {
                 txn_reply = (struct binder_transaction_data *)(buffer + pos);
                 break;
             }
         }
-
-        // Skip unknown/unsupported binder responses
         pos += sizeof(struct binder_transaction_data);
     }
 
@@ -166,7 +166,7 @@ jobject receive_media_projection_reply(int binder_fd, JNIEnv *env) {
         return NULL;
     }
 
-    uintptr_t data_ptr = (uintptr_t)txn_reply->data.ptr.buffer;
+    uintptr_t data_ptr = (uintptr_t)txn_reply->data.ptr.ptr;
     int32_t binder_handle = *((int32_t *)data_ptr);
 
     if (binder_handle == 0) {
