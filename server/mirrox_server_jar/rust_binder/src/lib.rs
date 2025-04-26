90244d41d7848b211596029b56fbe397db2ff0ed
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-26 22:20:50 +0530

    lib.rs updated with the main logic, Rusty way initiated

---
+++ server/mirrox_server_jar/rust_binder/src/lib.rs
@@ -0,0 +1,45 @@
+use std::os::unix::io::RawFd;
+use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
+use nix::unistd::{read, write};
+use jni::JNIEnv;
+use jni::objects::JClass;
+use jni::sys::jobject;
+use log::error;
+
+#[no_mangle]
+pub extern "system" fn Java_com_mirrox_server_StartMirrox_nativeGetMediaProjection(
+    env: JNIEnv,
+    _class: JClass,
+) -> jobject {
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_min_level(log::Level::Debug)
+            .with_tag("MirrOxRust"),
+    );
+
+    match open_binder() {
+        Ok(binder_fd) => {
+            if send_create_projection_transaction(binder_fd).is_ok() {
+                // TODO: receive reply
+                error!("Success sending transaction!");
+            } else {
+                error!("Failed to send createProjection");
+            }
+        }
+        Err(e) => {
+            error!("Failed to open binder device: {:?}", e);
+        }
+    }
+
+    std::ptr::null_mut()
+}
+
+fn open_binder() -> nix::Result<RawFd> {
+    open("/dev/binder", OFlag::O_RDWR | OFlag::O_CLOEXEC, Mode::empty())
+}
+
+fn send_create_projection_transaction(_binder_fd: RawFd) -> nix::Result<()> {
+    // TODO: Implement Binder protocol here
+    Ok(())
+}
