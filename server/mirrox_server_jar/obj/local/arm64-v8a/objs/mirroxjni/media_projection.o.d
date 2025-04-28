df59eda175e6221e4d7afe064e2dfa92e9c9a1c9
Sampreet Patil <sampreetpatil270@gmail.com>
2025-04-28 19:51:37 +0530

    old .so ndk-build cleaned

--- server/mirrox_server_jar/obj/local/arm64-v8a/objs/mirroxjni/media_projection.o.d
+++
@@ -1,3 +0,0 @@
-./obj/local/arm64-v8a/objs/mirroxjni/media_projection.o: \
-  jni/media_projection.c jni/binder_utils.h
-jni/binder_utils.h:
--- server/mirrox_server_jar/obj/local/x86_64/objs/mirroxjni/binder_utils.o.d
+++
@@ -1,9 +0,0 @@
-./obj/local/x86_64/objs/mirroxjni/binder_utils.o: jni/binder_utils.c \
-  jni/include/binder.h jni/include/binderfs.h jni/include/ioctl.h \
-  jni/include/types.h jni/include/binder_version.h jni/binder_utils.h
-jni/include/binder.h:
-jni/include/binderfs.h:
-jni/include/ioctl.h:
-jni/include/types.h:
-jni/include/binder_version.h:
-jni/binder_utils.h:
