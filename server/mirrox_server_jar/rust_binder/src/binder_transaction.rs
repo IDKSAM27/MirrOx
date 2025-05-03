62ac02536a612ff9b017e984110c3b948087acfa
Sampreet Patil <sampreetpatil270@gmail.com>
2025-05-03 16:06:40 +0530

    BINDER_WRITE_READ initalised as u64, converted to i32

--- server/mirrox_server_jar/rust_binder/src/binder_transaction.rs
+++ server/mirrox_server_jar/rust_binder/src/binder_transaction.rs
@@ -6,7 +6,8 @@ use std::ffi::CString;
 use nix::unistd::geteuid;
 
 // Required ioctl number for BINDER_WRITE_READ
-const BINDER_WRITE_READ: u64 = 0xC0306201;
+// const BINDER_WRITE_READ: u64 = 0xC0306201;
+const BINDER_WRITE_READ: u64 = 0xC0306201u32 as i32;
 const BR_TRANSACTION: u32 = 0x1c;
 const BR_REPLY: u32 = 0x1f;
 
