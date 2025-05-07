ea430e36831e1bd49ff8859029f36562ba2da973
Sampreet Patil <sampreetpatil270@gmail.com>
2025-05-07 12:45:03 +0530

    dex_jar shelling done

---
+++ server/mirrox_server_jar/dex_jar.sh
@@ -0,0 +1,45 @@
+set -e
+
+# CONFIG
+D8_BAT="C:\Users\Sampreet\AppData\Local\Android\Sdk\build-tools\35.0.0\d8.bat"
+
+echo "
+[*] Cleaning Gradle..."
+
+./gradlew clean
+
+
+echo "
+[*] Gradle Building..."
+
+./gradlew buildMirroxJar 
+
+
+echo "
+[*] mkdir build/dex"
+
+mkdir build/dex
+
+
+echo "
+[*] Dexxing the Jar..."
+
+"$D8_BAT" --output=build/dex build/libs/mirrox_server-1.0.jar
+
+
+echo "
+[*] Going to build/dex..."
+
+cd build/dex
+
+
+echo "
+[*] Jarring the classes.dex..."
+
+jar cf mirrox_server.dex.jar classes.dex
+
+cd ..
+cd ..
+
+echo "
+[✅] mirrox_server.dex.jar is ready !!!"
