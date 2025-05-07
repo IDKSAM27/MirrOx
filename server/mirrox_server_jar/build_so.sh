1511953b60dcb3777820473fa793f9cf41701330
Sampreet Patil <sampreetpatil270@gmail.com>
2025-05-07 13:04:00 +0530

    final .sh's update

--- server/mirrox_server_jar/build_so.sh
+++ server/mirrox_server_jar/build_so.sh
@@ -3,12 +3,36 @@ set -e
 
 # CONFIG
 
+echo "
+[*] Adding aarch64-linux-android as target..."
+
 rustup target add aarch64-linux-android
+
+
+echo "
+[*] Installing cargo-ndk..."
 
 cargo install cargo-ndk
 
+
+echo "
+[*] Going to rust_binder..."
+
 cd rust_binder
+
+
+echo "
+[*] Cleaning cargo ndk..."
 
 cargo ndk clean
 
+
+echo "
+[*] Building cargo ndk"
+
 cargo ndk -t arm64-v8a -o ./target/ --platform 21 build
+
+cd ..
+
+echo "
+[✅] librust_binnder.so is ready !!!" 
--- server/mirrox_server_jar/run_mirrox.sh
+++ server/mirrox_server_jar/run_mirrox.sh
@@ -8,13 +8,16 @@ JAR_NAME="mirrox_server.dex.jar"
 DEVICE_PATH="/data/local/tmp"
 
 # === STEP 1: Push .so to device ===
-echo "📦 Pushing native .so to device..."
+echo "
+📦 Pushing native .so to device..."
 adb push ../rust_binder/target/$ARCH/$LIB_NAME $DEVICE_PATH/
 
 # === STEP 2: Push .jar to device ===
-echo "📦 Pushing JAR to device..."
+echo "
+📦 Pushing JAR to device..."
 adb push build/dex/$JAR_NAME $DEVICE_PATH/
 
 # === STEP 3: Run the server using app_process ===
-echo "🚀 Launching mirrox_server using app_process..."
+echo "
+🚀 Launching mirrox_server using app_process..."
 adb shell CLASSPATH=$DEVICE_PATH/$JAR_NAME app_process / com.mirrox.server.StartMirrox
