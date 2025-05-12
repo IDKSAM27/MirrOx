d1bc516935aee60d6c3283a60ea143a8c2872cf5
Sampreet Patil <sampreetpatil270@gmail.com>
2025-05-12 22:37:52 +0530

    github api set for grep'n latest server .jar

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -7,3 +7,6 @@ OUTPUT_DIR = "mirrox/server"
 mkdir -p "$OUTPUT_DIR"
 
 echo "Fetching latest scrcpy version..."
+
+# Fetch latest version tag using GitHub API
+LATEST_TAG = $(curl -s https://api.github.com/repos/Genymobile/scrcpy/releases/latest | grep -oP ' "tag_name": "\K(.*)(?=")')
