bb45ee273f5c18f22fa4b37a29abdf876c7b1a69
Sampreet Patil <sampreetpatil270@gmail.com>
2025-05-13 13:05:17 +0530

    finally no more server version from client issue

--- src/adb.rs
+++ src/adb.rs
@@ -4,7 +4,7 @@ use std::io::{BufReader, BufRead};
 pub fn start_scrcpy_server() -> std::io::Result<()> {
     // Push server JAR
     Command::new("adb")
-        .args(["push", "scrcpy/scrcpy-server.jar", "/data/local/tmp/scrcpy-server.jar"])
+        .args(["push", "server/scrcpy-server.jar", "/data/local/tmp/scrcpy-server.jar"])
         .status()?;
 
     // Set up reverse tunnel (server will listen on localabstract:scrcpy)
@@ -20,7 +20,8 @@ pub fn start_scrcpy_server() -> std::io::Result<()> {
             "app_process",
             "/",
             "com.genymobile.scrcpy.Server",
-            "1.25",
+            // "1.25",
+            "3.2",
             "log_level=info",
         ])
         .stdout(Stdio::piped())
