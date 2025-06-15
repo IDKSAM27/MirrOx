5a5e39adb590efeb99de5b1c196f2d05dafcb898
Sampreet Patil <sampreetpatil270@gmail.com>
2025-06-15 20:00:44 +0530

    a bit main refining

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 mod adb;
 mod mux;
+mod tcp_stream;
 mod video;
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // 1. Start scrcpy server over ADB
-    adb::start_scrcpy_server("v3.2")?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server_version = "v3.2";
 
-    // 2. Delay a bit to allow the server to boot
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    adb::start_scrcpy_server(server_version)?;
+    let _tcp_stream = tcp_stream::connect_to_server()?; // Just to trigger server handshake
 
-    // 3. Start TCP stream and get video byte receiver
-    let receiver = mux::start_muxed_stream()?;
-
-    // 4. Start decoding and rendering
-    video::start_video_stream(receiver)?;
+    let video_receiver = mux::start_muxed_stream()?;
+    video::start_video_stream(video_receiver)?;
 
     Ok(())
 }
